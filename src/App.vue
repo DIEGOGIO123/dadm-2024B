c9d691d451ceb567b5e22a81da29afc2ddc21515
DIEGOGIO123 <159564293+DIEGOGIO123@users.noreply.github.com>
2024-10-02 17:47:57 +0000

    ⭐S11 Methods add method to add item and clean input

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,55 @@
 <script setup>
 import { ref } from 'vue';
-//Modelo
-const header = ref  ('App lista de compras');
-const items = ref ([
-{id: '0', label: '10 bolillos'},  
-{id: '1', label:'1 lata frijoles'},
-{id: '2', label: '1 Chelas'},
-{id: '3', label: '1 nutella'},
+// Modelo
+const header = ref('App lista de compras');
+//---items----
+//Item-model
+const items = ref([
+  {id:'0', label: '10 bolillos'},
+  {id:'1', label: '1 chela'},
+  {id:'2', label: 'leche'},
+  {id:'3', label: '1 nutella'}
 ]);
-const newItem = ref('');
-const newItemPriority = ref('low');
+//Item-Method
+const saveItem = ()=> {
+  //Add new item
+  items.value.push({id: items.value.length+1,label : newItem.value});
+  //Limpia el input
+  newItem.value ='';
+};
+const newItem = ref("");
+const newItemHighPriority = ref(false);
+
 </script>
-
+ 
 <template>
-<h1> 
-    <i class="material-icons shopping-cart-icon"> local_mall</i>
-    {{ header }} 
-    </h1>
-    <input v-model.lazy="newItem" type="text" placeholder="Agregar Articulo">
-    <!--Radio Buttons-->
-    <label><input type="radio" value="low" v-model="newItemPriority">baja</label>
-    <label><input type="radio" value="high" v-model="newItemPriority">alta</label>
-    {{ newItemPriority == 'low' ? '🧊' : '🔥' }}    
-    <ul>
-        <li v-for= "({id, label}, i) in items" :key = "id">{{i+1}} {{ i%2==0?'🎇': '👜'}} {{ label }} </li>
-    </ul>
+  <h1>
+   <i class="material-icons shopping-cart-icon">local_mall</i>
+    {{ header }}
+  </h1>
+  <form 
+   class="add-item form"
+   v-on:submit.prevent="saveItem()">
+  <input v-model="newItem" type="text" placeholder="Agregar un articulo" />
+  <!--Caja de seleccion de Prioridad-->
+  <label>
+    <input type="checkbox" v-model="newItemHighPriority" />
+    Alta Prioridad
+  </label>
+  <!--Boton-->
+  <button class="btn btn-primary">
+    Salvar Articulo
+  </button>
+  </form>
+  <ul></ul>
+ 
+  <ul>
+    <li v-for="item in items" :key="item.id"> 🛍 {{  item.label }} </li>
+  </ul>
 </template>
-
+ 
 <style scoped>
-.shopping-cart-icon {
-font-size: 2rem;
+.shopping-cart-icon{
+  font-size: 2rem;
 }
 </style>

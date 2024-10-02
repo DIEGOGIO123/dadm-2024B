0c9effa33463f834f8db34542311760891f5cb0d
DIEGOGIO123 <159564293+DIEGOGIO123@users.noreply.github.com>
2024-10-02 18:47:54 +0000

    ⭐S12 Introduction v-if Shows and hides main form

--- src/App.vue
+++ src/App.vue
@@ -17,18 +17,39 @@ const saveItem = ()=> {
   //Limpia el input
   newItem.value ='';
 };
+// --- FORMULARIO ---
 const newItem = ref("");
 const newItemHighPriority = ref(false);
+const editing = ref(true);
+//--- EVENTOS ---
+const activateEdition =(activate) => {
+  editing.value = activate
+};
 
 </script>
  
 <template>
-  <h1>
+  <div class="header">
+    <h1>
    <i class="material-icons shopping-cart-icon">local_mall</i>
-    {{ header }}
+   {{ header }}
   </h1>
+  <button 
+  v-if="editing" 
+  class="btn" 
+  @click="activateEdition(false)">
+  Cancelar
+</button>
+  <button 
+  v-else 
+  class="btn btn-primary" 
+  @click="activateEdition(true)">
+  Agregar Articulo
+</button>
+  </div>
   <form 
    class="add-item form"
+   v-if="editing"
    v-on:submit.prevent="saveItem()">
   <input v-model="newItem" type="text" placeholder="Agregar un articulo" />
   <!--Caja de seleccion de Prioridad-->
@@ -41,11 +62,14 @@ const newItemHighPriority = ref(false);
     Salvar Articulo
   </button>
   </form>
-  <ul></ul>
+  <ul>
+
+  </ul>
  
   <ul>
     <li v-for="item in items" :key="item.id"> 🛍 {{  item.label }} </li>
   </ul>
+  <p v-if="items.length === 0">🥀NO HAY ELEMENTOS EN LA LISTA 🥀</p>
 </template>
  
 <style scoped>

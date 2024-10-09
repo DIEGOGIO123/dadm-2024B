a2376da44f9bd9253563a0e61d1c2d18689045a9
DIEGOGIO123 <159564293+DIEGOGIO123@users.noreply.github.com>
2024-10-09 17:21:12 +0000

    ⭐S13 Entrega Condicional Deshabilita el boton cuando el input esta vacio

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,71 @@
 <script setup>
 import { ref } from 'vue';
+
 // Modelo
 const header = ref('App lista de compras');
-//---items----
-//Item-model
+
+// ---items----
 const items = ref([
-  {id:'0', label: '10 bolillos'},
-  {id:'1', label: '1 chela'},
-  {id:'2', label: 'leche'},
-  {id:'3', label: '1 nutella'}
+  { id: '0', label: '10 bolillos' },
+  { id: '1', label: '1 chela' },
+  { id: '2', label: 'leche' },
+  { id: '3', label: '1 nutella' }
 ]);
-//Item-Method
-const saveItem = ()=> {
-  //Add new item
-  items.value.push({id: items.value.length+1,label : newItem.value});
-  //Limpia el input
-  newItem.value ='';
+
+// Item-Method
+const saveItem = () => {
+  // Add new item
+  items.value.push({ id: items.value.length + 1, label: newItem.value });
+  newItem.value = ""; // Limpiar el input después de agregar
 };
-// --- FORMULARIO ---
+
+// --Formulario---
 const newItem = ref("");
 const newItemHighPriority = ref(false);
 const editing = ref(true);
-//--- EVENTOS ---
-const activateEdition =(activate) => {
-  editing.value = activate
+const activeEdition = (activate) => {
+  editing.value = activate;
 };
-
 </script>
- 
+
 <template>
   <div class="header">
     <h1>
-   <i class="material-icons shopping-cart-icon">local_mall</i>
-   {{ header }}
-  </h1>
-  <button 
-  v-if="editing" 
-  class="btn" 
-  @click="activateEdition(false)">
-  Cancelar
-</button>
-  <button 
-  v-else 
-  class="btn btn-primary" 
-  @click="activateEdition(true)">
-  Agregar Articulo
-</button>
+      <i class="material-icons shopping-cart-icon">local_mall</i>
+      {{ header }}
+    </h1>
+    <button v-if="editing" class="btn" @click="activeEdition(false)">
+      Cancelar
+    </button>
+    <button v-else class="btn btn-primary" @click="activeEdition(true)">
+      Agregar articulo
+    </button>
   </div>
-  <!--Colocando un hyperlink-->
-  <a v-bind:href="'https://' + newItem" target="_blank">{{ newItem == "" ? "🔗 Link": newItem }}</a>
-  {{ 'https://' + newItem }}
-  <form 
-   class="add-item form"
-   v-if="editing"
-   v-on:submit.prevent="saveItem()">
-  <input v-model="newItem" type="text" placeholder="Agregar un articulo" />
-  <!--Caja de seleccion de Prioridad-->
-  <label>
-    <input type="checkbox" v-model="newItemHighPriority" />
-    Alta Prioridad
-  </label>
-  <!--Boton-->
-  <button class="btn btn-primary">
-    Salvar Articulo
-  </button>
+
+  <!-- Agrupando Entradas de usuario -->
+  <form class="add-item form" v-if="editing" v-on:submit.prevent="saveItem">
+    <!-- Entrada de texto -->
+    <input v-model="newItem" type="text" placeholder="Agregar un articulo" />
+    <!-- Caja de seleccion de Prioridad -->
+    <label>
+      <input type="checkbox" v-model="newItemHighPriority" />
+      Alta Prioridad
+    </label>
+    <!-- Boton -->
+    <button 
+    :disabled="newItem.length == 0"
+    class="btn btn-primary">
+      Salvar Articulo
+    </button>
   </form>
   <ul>
-
+    <li v-for="item in items" :key="item.id"> 🛍 {{ item.label }} </li>
   </ul>
- 
-  <ul>
-    <li v-for="item in items" :key="item.id"> 🛍 {{  item.label }} </li>
-  </ul>
-  <p v-if="items.length === 0">🥀NO HAY ELEMENTOS EN LA LISTA 🥀</p>
+  <p v-if="items.length === 0"> 🥀 NO HAY ELEMENTOS EN LA LISTA 🥀</p>
 </template>
- 
+
 <style scoped>
-.shopping-cart-icon{
+.shopping-cart-icon {
   font-size: 2rem;
 }
 </style>
